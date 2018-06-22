242885397648d79fc6544348abf2fe0ed4c41801
Dannes Wessels <dannes@exist-db.org>
2018-06-22 22:13:14 +0200

    Add tests

--- src/test/java/net/trundler/MyJunitTest.java
+++ src/test/java/net/trundler/MyJunitTest.java
@@ -1,4 +1,12 @@
 package net.trundler;
 
+import org.exist.test.runner.XSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(XSuite.class)
+@XSuite.XSuiteFiles({
+        "src/test/resources/xquery"
+})
+
 public class MyJunitTest {
 }
